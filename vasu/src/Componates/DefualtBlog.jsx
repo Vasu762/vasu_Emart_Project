78e54890bba843a7a85cfda7eaafb4d8a8e65716
Vasu762 <sq.vaghasiyavasu10@gmail.com>
2024-04-27 09:24:27 +0530

    add files

---
+++ vasu/src/Componates/DefualtBlog.jsx
@@ -0,0 +1,163 @@
+import React from 'react'
+
+const DefualtBlog = () => {
+    return (
+
+
+        <div className='container m-auto '>
+
+
+            <div className='flex justify-around flex-wrap mt-16'>
+                <div className='inline-block overflow-hidden'>
+                    <img className='hover:scale-110 duration-500 transition-transform  max-w-[700px] ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-img-12-1.webp" alt="" />
+                </div>
+
+
+
+                <div>
+                    <input className='border-2 border-solid border-black mt-4' type="text" name="" id="" />
+                    <p className='mt-3 font-bold text-xl'> Categories</p>
+                    <input className='hover:    mt-3' type="radio" name="" id="" /><label htmlFor="">Beauty</label><br />
+                    <input className='hover:   mt-1' type="radio" name="" id="" /><label htmlFor="">Consulting</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Creative</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Design</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Fashion</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Modern</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Season Style</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Shoping</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Start Up</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Style</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Style Development</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Success Cases</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">Trending</label><br />
+
+
+                    <p className='text-lg font-bold mt-3'> Archives</p>
+
+                    <input className='hover: mt-3' type="radio" name="" id="" /><label htmlFor="">October 2022</label><br />
+                    <input className='hover: mt-1' type="radio" name="" id="" /><label htmlFor="">May 2021</label><br />
+
+                    <p className='text-lg font-bold mt-3'>Tag</p>
+                    <div className='mt-3'>
+                        <button className='border-2 border-r-neutral-600 border-solid p-1 px-3 rounded-full bg-slate-200 hover:bg-black hover:text-white m-1'>Creativ</button>
+                        <button className='border-2 border-r-neutral-600 border-solid p-1 px-3 rounded-full bg-slate-200 hover:bg-black hover:text-white m-1'>Design</button>
+                        <button className='border-2 border-r-neutral-600 border-solid p-1 px-3 rounded-full bg-slate-200 hover:bg-black hover:text-white m-1'>Fashion</button>
+                        <button className='border-2 border-r-neutral-600 border-solid p-1 px-3 rounded-full bg-slate-200 hover:bg-black hover:text-white m-1'>Tread</button>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+
+            {/* image1 */}
+            <div className='flex container gap-5 ms-6 flex-wrap'>
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-three.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Leota Jerez</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>5 Incredibly Useful Fashion Tips  For <br /> Small Businesses</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-one.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Leota Jerez</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>How To Find The Right Fashion For <br /> Your Specific Product</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+            </div>
+
+            {/* image2 */}
+            <div className='flex container gap-5 ms-6 flex-wrap'>
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-four.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>William Westbrook</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>The Psychology Behind Fashion <br />Trends: What We Wear</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-five.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Jessica Bryant</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>How I Improved My Fashion In One <br /> Easy Lesson</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+            </div>
+
+
+            {/* image3 */}
+
+            <div className='flex container gap-5 ms-6 flex-wrap'>
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-six.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Leota Jerez</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>From Runway To Reality: The <br />Latest Fashion Trends</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-seven.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Leota Jerez</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>How to Mix & Match Your Clothes <br />for Endless Outfit Options</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+            </div>
+
+
+
+            {/* image4 */}
+            <div className='flex container gap-5 ms-6 flex-wrap'>
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-eight.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Leota Jerez</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>10 Must Have Basics For Every <br />Woman’s Closet</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>Never ever think of giving up. Winners never quit <br /> and quitters never win for slogan...</p>
+                </div>
+
+                <div className='overflow-hidden inline-block'>
+                    <img className='max-w-96 hover:scale-110 duration-500 transition-transform ' src="https://emart.wpthemedemos.com/wp-content/uploads/2022/10/blog-image-ten.webp" alt="" />
+                    <p className='mt-3 inline-block font-bold text-slate-500'>Leota Jerez</p
+                    >
+                    <div class="h-3 border-l font-bold border-gray-400 inline-block ms-2"></div>
+                    <p className='inline-block ms-2 text-red-600 font-bold'>19 Oct, 2022</p>
+                    <p className=' font-bold text-wrap text-xl'>How To Find The Right Fashion For <br /> Your Specific Product</p>
+                    <p className='font-semibold  text-slate-500 mt-3'>What makes a slogan or a tagline for an app  <br /> development company....</p>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default DefualtBlog
